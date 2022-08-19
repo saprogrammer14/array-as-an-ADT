dcac27fc502701d3bc09631a2970e686f35439f4
saprogrammer14 <shoumik.khowai@gmail.com>
2022-08-19 18:22:26 +0530

    added readme

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+// A test program
 #include "array.cpp"
 
 int main()
 {
     Array<int> a(5);
-    a[1]=2;
+    a[1] = 2;
     a.display();
 
     Array<float> b(6);
---
+++ readme.md
@@ -0,0 +1,10 @@
+
+# Array as an abstract data type
+
+
+
+
+## Note
+
+If anyone knows how to compile multiple cpp files and link obj files please let me know.
+
